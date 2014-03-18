cdc8e2d0955091b8bebfd35b15ff00fadf4df25e
Bart Van Itterbeeck <bartvanitterbeeck@gmail.com>
2014-03-18 21:40:31 +0100

    basic

--- app.js
+++ app.js
@@ -1,39 +1,51 @@
-// Import the Express module
-var express = require('express');
+// // Import the Express module
+// var express = require('express');
 
-// Import the 'path' module (packaged with Node.js)
-var path = require('path');
+// // Import the 'path' module (packaged with Node.js)
+// var path = require('path');
 
-// Create a new instance of Express
-var app = express();
+// // Create a new instance of Express
+// var app = express();
 
-// Import the Anagrammatix game file.
-var game = require('./game');
+// // Import the Anagrammatix game file.
+// var game = require('./game');
 
-// Create a simple Express application
-app.configure(function() {
-    // Turn down the logging activity
-    app.use(express.logger('dev'));
+// // Create a simple Express application
+// app.configure(function() {
+//     // Turn down the logging activity
+//     app.use(express.logger('dev'));
 
-    // Serve static html, js, css, and image files from the 'public' directory
-    app.use(express.static(path.join(__dirname,'public')));
-});
+//     // Serve static html, js, css, and image files from the 'public' directory
+//     app.use(express.static(path.join(__dirname,'public')));
+// });
 
-app.get('/',function(req,res){
-	res.send('app is running <br><br><br>&copy;Kerm.is 2014')
-})
+// app.get('/',function(req,res){
+// 	res.send('app is running <br><br><br>&copy;Kerm.is 2014')
+// })
 
-// Create a Node.js based http server on port 8080
-var server = require('http').createServer(app).listen(8080);
+// // Create a Node.js based http server on port 8080
+// var server = require('http').createServer(app).listen(8080);
 
-// Create a Socket.IO server and attach it to the http server
-var io = require('socket.io').listen(server);
+// // Create a Socket.IO server and attach it to the http server
+// var io = require('socket.io').listen(server);
 
-// Reduce the logging output of Socket.IO
-io.set('log level',1);
+// // Reduce the logging output of Socket.IO
+// io.set('log level',1);
 
-// Listen for Socket.IO Connections. Once connected, start the game logic.
+// // Listen for Socket.IO Connections. Once connected, start the game logic.
+// io.sockets.on('connection', function (socket) {
+//     //console.log('client connected');
+//     game.initGame(io, socket);
+// });
+
+var io = require('socket.io').listen(80);
+
 io.sockets.on('connection', function (socket) {
-    //console.log('client connected');
-    game.initGame(io, socket);
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+  socket.on('new room', function(data){
+  	console.Log('new room', data)
+  })
 });
